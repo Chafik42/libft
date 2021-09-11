09d939abc88a36a5842e4610d2e66d650f004fd5
kika75020 <qatar75020@gmail.com>
2021-09-11 14:01:03 +0200

    Partie 1 FINI

--- libft/srcs/ft_substr.c
+++ libft/srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cmarouf <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 20:52:44 by cmarouf           #+#    #+#             */
-/*   Updated: 2021/09/11 04:43:16 by cmarouf          ###   ########.fr       */
+/*   Updated: 2021/09/11 13:58:09 by cmarouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,6 +19,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	j = 0;
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (ft_strlen(&s[start]) < len)
+		len = ft_strlen(&s[start]);
 	dest = malloc(sizeof(char) * (len) + 1);
 	if (!dest)
 		return (NULL);
